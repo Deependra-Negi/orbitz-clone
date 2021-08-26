173ec99866b70bac4c75cb0312149038403011ce
Deependra-Negi <916deepaknegi@gmail.com>
2021-08-26 20:10:19 +0530

    room-card added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 
 import { Routes } from './Routes/Routes';
-import RoomCard from './Components/RoomCard/RoomCard'
+//import RoomCard from './Components/RoomCard/RoomCard'
 
 
 function App() {
   return (
     <>
-      <RoomCard/>
-      {/* <Navbar />
-      <Routes /> */}
+      {/* <RoomCard/> */}
+      <Navbar />
+      <Routes />
     </>
   )
 }

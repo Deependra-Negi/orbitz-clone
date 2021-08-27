2f453fc55187009dd6cf7aab196e85acc6aeea7d
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-08-28 01:55:31 +0530

    help bot done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import HelpBoat from './Components/HelpBot/HelpBoat'
 import { Routes } from './Routes/Routes';
-
-
-
 
 function App() {
   return (
     <>
       <Navbar/>
  <Routes/>
-    
-    
-    </>
+    <HelpBoat/>
+  </>
   );
 }
 

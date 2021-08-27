4216ed02e45c9b97c473e79844f7b2ae34f3cb93
pawanpatidar21 <pawanpatidar21@gmail.com>
2021-08-27 16:44:06 +0530

    allaminities

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 
 import { Routes } from './Routes/Routes';
-//import RoomCard from './Components/RoomCard/RoomCard'
 
-import RewardCard from './Components/RewardCard/RewardCard';
-import ForgotPassword from './Components/SignUpAndSignIn/Forgot/ForgotPassword';
+
 
 function App() {
   return (
     <>
-       <Navbar />
-      <Routes />
-      
-      
-    
-{/* 
-      <Sort />
-      <HotelListFilters />  */}
+ <Routes/>
     
     
     </>
